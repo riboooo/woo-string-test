e2045af47a63bebfd4b045cd127d334a394b944e
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 08:17:57 +0900

    요구사항3 구현 - "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습테스트를 구현한다. - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다. - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.

--- src/test/java/com/example/study/StringTest.java
+++ src/test/java/com/example/study/StringTest.java
@@ -1,5 +1,6 @@
 package com.example.study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
@@ -32,5 +33,47 @@ public class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
+    @Test
+    @DisplayName("특정 위치 문자 가져오기")
+    public void charAtTest(){
+        // given
+        String target = "abc";
+        int index = 2;
 
+        // when
+        char c = target.charAt(index);
+
+        // then
+        assertThat(c).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt assertThatThrownBy Exception 처리 테스트")
+    public void charAtExceptionTest(){
+        // given
+        String target = "abc";
+        int index = 30;
+
+        // when && then
+        assertThatThrownBy(() -> {
+                    char c = target.charAt(index);
+                    assertThat(c).isEqualTo('c');
+                }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("%d", index);
+    }
+
+    @Test
+    @DisplayName("charAt assertThatExceptionOfType Exception 처리 테스트")
+    public void charAtExceptionOfTypeTest(){
+        // given
+        String target = "abc";
+        int index = 30;
+
+        // when && then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char c = target.charAt(index);
+                    assertThat(c).isEqualTo('c');
+                }).withMessageContaining("%d", index);
+    }
 }
