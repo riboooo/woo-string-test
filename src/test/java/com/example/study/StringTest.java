ad652c05642d90c2128e108ae2f84578cbb2b710
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 08:03:46 +0900

    요구사항1 구현 -"1,2"을 , 로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다 -"1"을 , 로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.

--- src/test/java/com/example/study/StringTest.java
+++ src/test/java/com/example/study/StringTest.java
@@ -5,4 +5,20 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
+    @Test
+    public void splitTest(){
+        // given
+        String target_1 = "1,2";
+        String target_2 = "1";
+
+        // when
+        String[] result_1 = target_1.split(",");
+        String[] result_2 = target_2.split(",");
+
+        // then
+        assertThat(result_1).contains("1","2");
+        assertThat(result_2).containsExactly("1");
+    }
+
+
 }
