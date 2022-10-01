3440ae2100df9103116255daaeeecfa6bacfd403
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 08:27:25 +0900

    Set Test - 요구사항2 - Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다. - 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다. - JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다

--- src/test/java/com/example/study/SetTest.java
+++ src/test/java/com/example/study/SetTest.java
@@ -3,10 +3,13 @@ package com.example.study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -24,10 +27,26 @@ public class SetTest {
     @DisplayName("Set 크기 확인")
     void checkSize() {
         // given && when
-        int size = numbers.size();짐
+        int size = numbers.size();
 
         // then
         assertThat(size).isEqualTo(3);
+    }
+
+
+    @DisplayName("Set 내부 값 확인 - 중복코드 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void usingContains(int nums) {
+
+        /*
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+        */
+
+        // then
+        assertTrue(numbers.contains(nums));
     }
 
 
